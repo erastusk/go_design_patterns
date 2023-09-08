5ad7a689ae05cc2fe25f0b16b2cd53cc68a73153
thamboe <erastus.thambo@broadridge.com>
2023-09-08 17:51:46 -0400

    Builder pattern

--- builder/main.go
+++ builder/main.go
@@ -6,6 +6,7 @@ func main() {
 	housebuilder := gethouseBuilder()
 	h := housebuilder.AddDoor("wood").AddFloors(3).AddWindow("glass")
 	fmt.Printf("%+v", h)
+	// OR
 	housebuilder_1 := housebuilder.buildHouse()
 	fmt.Printf("%+v", housebuilder_1)
 }
